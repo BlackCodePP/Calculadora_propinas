b9ea23870634be667ee5bba3c551e72246141165
Pedro Panisello <pedropanisello23@gmail.com>
2024-06-27 17:15:19 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <header className=" bg-teal-400 py-5">
+        <h1 className=" text-center text-4xl font-black">Calculadora de propinas y Consumo</h1>
+      </header>
+    </>
+  );
+}
+
+export default App;
