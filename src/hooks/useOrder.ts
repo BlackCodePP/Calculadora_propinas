06de820a71d0dcb738a005d372493728a0c82fd4
Pedro Panisello <pedropanisello23@gmail.com>
2024-07-01 12:21:33 +0200

    check cart duplicates and add quantity

--- src/hooks/useOrder.ts
+++ src/hooks/useOrder.ts
@@ -5,8 +5,16 @@ export default function useOrder () {
     const [order,setOrder] = useState<TOrdetItem[]>([])
 
     const addItem = (item: TMenuItem) => {
-        const newItem : TOrdetItem = {...item, quantity: 1}
-        setOrder([...order, newItem])
+        const itemExist = order.find(orderItem => orderItem.id === item.id)
+        if (itemExist) {
+            const updatedOrder = order.map (orderItem => orderItem.id === item.id ? 
+                {...orderItem, quantity:orderItem.quantity + 1} : orderItem
+            )
+            setOrder(updatedOrder)
+        } else {
+            const newItem : TOrdetItem = {...item, quantity: 1}
+            setOrder([...order, newItem])
+        }
     }
 
     console.log (order)
